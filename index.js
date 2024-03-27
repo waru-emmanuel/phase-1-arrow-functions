fd42051edbb89a44ba2c5bdf07ffe0054dc1db8b
wachira <wewaruts@gmail.com>
2024-03-28 00:25:41 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(a,b){
+    return a/b;
+}
+ const square = parameter1 =>{
+ return parameter1 * parameter1
+}
+
+const add = (parameter1, parameter2) =>{
+    return parameter1 + parameter2
+   }
+
